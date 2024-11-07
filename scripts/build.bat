2ae75ac15118657d054db2723f33f7650a9ae74b
jamesnulliu <jamesnulliu@gmail.com>
2024-11-07 19:52:40 +0800

    Update launch and task

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,19 +1,17 @@
 @echo off
 
+set "ARCH=x86_amd64"
+set "BUILD_TYPE=%1"
+
 set "VS2022_PATH=C:\Program Files\Microsoft Visual Studio\2022\Community"
 if not exist "%VS2022_PATH%" (
     echo Visual Studio 2022 not found at: %VS2022_PATH%
     exit /b 1
 )
-
 set "VCVARSALL=%VS2022_PATH%\VC\Auxiliary\Build\vcvarsall.bat"
-
-set "ARCH=x86_amd64"
-if not "%1" == "" (
-    set "ARCH=%1"
-)
-
 echo Setting up Visual Studio environment for %ARCH%...
 call "%VCVARSALL%" %ARCH%
 
-call "bash"
+
+call cmake -G Ninja -S . -B ./build -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
+call cmake --build ./build -j %NUMBER_OF_PROCESSORS%
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,6 @@
+BuildType=$1
+
 cmake -G Ninja -S . -B ./build \
-    -DCMAKE_BUILD_TYPE=Release  
+    -DCMAKE_BUILD_TYPE=$BuildType  
 
 cmake --build ./build -j $(nproc)
