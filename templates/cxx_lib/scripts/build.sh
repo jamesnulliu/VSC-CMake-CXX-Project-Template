9f891e6338aac3b2ad53e0a560d02a8cd1d510b7
jamesnulliu <jamesnulliu@gmail.com>
2024-11-22 13:56:43 +0800

    Update Template Selection (#4)
    * Update Template Selection
    
    * Fix ENV:CUDA_HOME
    
    * Failed to Add CUDA Action Support

---
+++ templates/cxx_lib/scripts/build.sh
@@ -0,0 +1,35 @@
+# Env Variables: CC, CXX, NVCC_CCBIN
+
+source ./scripts/windows-prune-PATH.sh
+
+SOURCE_DIR=.
+BUILD_DIR=./build
+BUILD_TYPE=Release
+CXX_STANDARD=20
+BUILD_SHARED_LIBS=OFF
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -S|--source-dir)
+            SOURCE_DIR=$2; shift ;;
+        -B|--build-dir)
+            BUILD_DIR=$2; shift ;;
+        Release|Debug)
+            BUILD_TYPE=$1 ;;
+        --stdc++=*)
+            CXX_STANDARD="${1#*=}" ;;
+        --shared)
+            BUILD_SHARED_LIBS=ON ;;
+        *)
+            # [TODO] Add detailed help message
+            echo "Unknown argument: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+cmake -G Ninja -S $SOURCE_DIR -B $BUILD_DIR \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -DCMAKE_CXX_STANDARD=$CXX_STANDARD \
+    -DBUILD_SHARED_LIBS=$BUILD_SHARED_LIBS
+
+cmake --build $BUILD_DIR -j $(nproc)
