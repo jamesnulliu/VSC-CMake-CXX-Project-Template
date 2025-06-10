5114c87fbf220a71b4f8517f1de8b337570e8cf8
jamesnulliu <jamesnulliu@gmail.com>
2025-06-10 08:12:38 +0000

    Update gen script

--- .templates/gen_project.py
+++ .templates/gen_project.py
@@ -1,9 +1,10 @@
 from argparse import ArgumentParser, ArgumentTypeError
-from enum import Enum, unique
 import shutil
 from pathlib import Path
 import os
 import atexit
+import subprocess
+import re
 
 PROJECT_TYPE = {
     0: "cxx_exe",
@@ -44,6 +45,143 @@ TARGET_EXTENSIONS = (
 MATCH_PATTERN = "_template_project_name_"
 
 
+def parse_arguments():
+    parser = ArgumentParser(description="Project type selector")
+    parser.add_argument(
+        "--remove-templates",
+        action="store_true",
+        help="Remove '.templates' and '.github/workflows'.",
+    )
+    parser.add_argument(
+        "--reset",
+        action="store_true",
+        help="Reset template, remove all generated files",
+    )
+    parser.add_argument(
+        "-t",
+        "--project-type",
+        type=int,
+        choices=[t for t in range(len(PROJECT_TYPE))],
+        help=f"Type of project to create: {PROJECT_TYPE}",
+    )
+
+    def validate_project_name(name: str):
+        if not name.isidentifier():
+            raise ArgumentTypeError(
+                f"Project name '{name}' must be a valid identifier"
+            )
+        return name
+
+    parser.add_argument(
+        "-n",
+        "--project-name",
+        type=validate_project_name,
+        help="Name of the project. Affects namespace and the include directory name.",
+    )
+    args = parser.parse_args()
+    if not (args.reset or args.remove_templates) and (
+        args.project_type is None or args.project_name is None
+    ):
+        parser.error(
+            "--project-type and --project-name are required unless "
+            "--remove-templates or --reset is specified"
+        )
+
+    return args
+
+
+def get_cuda_arch():
+    cmd = """
+    if ! command -v nvcc &> /dev/null; then
+        echo "-1"
+        exit 1
+    fi
+
+    tmp_dir=$(mktemp -d)
+    cat << 'EOF' > "${tmp_dir}/check_arch.cu"
+#include <stdio.h>
+int main() {
+    int dev = 0;
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, dev);
+    printf("sm_%d%d", prop.major, prop.minor);
+    return 0;
+}
+EOF
+
+    if nvcc "${tmp_dir}/check_arch.cu" -o "${tmp_dir}/check_arch" &> \
+        /dev/null && "${tmp_dir}/check_arch" 2>/dev/null; then
+        rm -rf "$tmp_dir"
+        exit 0
+    else
+        rm -rf "$tmp_dir"
+        echo "-1"
+        exit 1
+    fi
+    """
+    result = subprocess.run(
+        ["bash", "-c", cmd], capture_output=True, text=True
+    )
+    return result.stdout.strip()
+
+
+def rcopy_files_from_src_dir_2_dst_dir(src_dir, dst_dir):
+    """
+    Recursively copy files from `src_dir` to `dst_dir`; The structure of the
+    directory tree is maintained.
+    """
+    shutil.copytree(src_dir, dst_dir, dirs_exist_ok=True)
+
+
+def replace_in_file(file_path: str, old: str, new: str):
+    file_path = Path(file_path)
+    with open(file_path, "r") as file:
+        content = file.read()
+    content = content.replace(old, new)
+    with open(file_path, "w") as file:
+        file.write(content)
+
+
+def remove_line_in_file(file_path: str, pattern: str):
+    file_path = Path(file_path)
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+    with open(file_path, "w") as file:
+        for line in lines:
+            if pattern not in line:
+                file.write(line)
+
+
+def get_linenos(file_path: str, pattern):
+    file_path = Path(file_path)
+    linenos = []
+    with open(file_path, "r") as file:
+        for lineno, line in enumerate(file.readlines()):
+            if pattern in line:
+                linenos.append(lineno)
+    return linenos
+
+
+def remove_lines_in_file(file_path: str, begin_lno: int, end_lno: int):
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+    end_lno = end_lno if end_lno > 0 else len(lines)
+    lines = [
+        l for lno, l in enumerate(lines) if lno < begin_lno or lno >= end_lno
+    ]
+    with open(file_path, "w") as file:
+        for l in lines:
+            file.write(l)
+
+
+def replace_for_files_in_dir(directory, old, new):
+    for root, _, files in Path(directory).walk():
+        for file in files:
+            if file.endswith(TARGET_EXTENSIONS):
+                file_path = Path(root, file)
+                replace_in_file(file_path, old, new)
+
+
 class ProjectGenerator:
     def __init__(self, project_name: str, project_type: str):
         self.project_name = project_name
@@ -51,63 +189,46 @@ class ProjectGenerator:
 
     def run(self):
         shutil.copytree(TEMPLATE_DIR / "common", ".", dirs_exist_ok=True)
-        shutil.copytree(TEMPLATE_DIR / self.project_type, ".", dirs_exist_ok=True)
-        Path("./include", MATCH_PATTERN).rename(Path("./include", self.project_name))
-        for directory in list(map(Path, ["include", "lib", "test", "src", "cmake"])):
-            self.replace_in_directory(directory, MATCH_PATTERN, self.project_name)
-        self.replace_api()
-        self.replace_in_file("CMakeLists.txt", MATCH_PATTERN, self.project_name)
+        shutil.copytree(
+            TEMPLATE_DIR / self.project_type, ".", dirs_exist_ok=True
+        )
+        Path("./include", MATCH_PATTERN).rename(
+            Path("./include", self.project_name)
+        )
+        for directory in list(
+            map(Path, ["include", "lib", "test", "src", "cmake"])
+        ):
+            replace_for_files_in_dir(
+                directory, MATCH_PATTERN, self.project_name
+            )
+        self.replace_cxx_api()
+        replace_in_file("CMakeLists.txt", MATCH_PATTERN, self.project_name)
+        # cuda exe or cuda lib
         if self.project_type.startswith("cuda"):
             if not (cuda_home := os.environ.get("CUDA_HOME", None)):
                 cuda_home = os.environ.get("CUDA_DIR", None)
-            self.replace_in_file(".clangd", "<path-to-cuda>", cuda_home)
+            if cuda_home is None:
+                raise RuntimeError("CUDA_HOME or CUDA_DIR is not found")
+            replace_in_file(".clangd", "<path-to-cuda>", cuda_home)
+            replace_in_file(".clangd", "<cuda-arch>", get_cuda_arch())
+        # c++ exe or c++ lib
         else:
-            self.remove_line_in_file(".clangd", "cuda")
+            remove_lines_in_file(".clangd", get_linenos(".clangd", "---"), -1)
+            remove_line_in_file(".clangd", "cuda")
 
-    @staticmethod
-    def replace_in_file(file_path: str, old: str, new: str):
-        file_path = Path(file_path)
-        with open(file_path, "r") as file:
-            content = file.read()
-        content = content.replace(old, new)
-        with open(file_path, "w") as file:
-            file.write(content)
-
-    @staticmethod
-    def remove_line_in_file(file_path: str, pattern: str):
-        file_path = Path(file_path)
-
-        # Read the content of the file
-        with open(file_path, "r") as file:
-            lines = file.readlines()
-
-        # Write back all lines except those containing 'pattern'
-        with open(file_path, "w") as file:
-            for line in lines:
-                if pattern not in line:
-                    file.write(line)
-
-    @staticmethod
-    def replace_in_directory(directory, old, new):
-        for root, _, files in Path(directory).walk():
-            for file in files:
-                if file.endswith(TARGET_EXTENSIONS):
-                    file_path = Path(root, file)
-                    ProjectGenerator.replace_in_file(file_path, old, new)
-
-    def replace_api(self):
-        self.replace_in_directory(
-            Path("include"),
+    def replace_cxx_api(self):
+        replace_for_files_in_dir(
+            "include",
             f"{self.project_name}_API",
             f"{self.project_name.upper()}_API",
         )
-        self.replace_in_directory(
-            Path("include"),
+        replace_for_files_in_dir(
+            "include",
             f"{self.project_name}_EXPORT",
             f"{self.project_name.upper()}_EXPORT",
         )
-        self.replace_in_directory(
-            Path("include"),
+        replace_for_files_in_dir(
+            "include",
             f"{self.project_name}_IMPORT",
             f"{self.project_name.upper()}_IMPORT",
         )
@@ -123,7 +244,9 @@ def main(args):
             README_PATH,
         ]
         atexit.register(
-            lambda: [shutil.rmtree(path, ignore_errors=True) for path in to_remove]
+            lambda: [
+                shutil.rmtree(path, ignore_errors=True) for path in to_remove
+            ]
         )
         return
 
@@ -145,48 +268,7 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="Project type selector")
-
-    parser.add_argument(
-        "--remove-templates",
-        action="store_true",
-        help="Remove '.templates' and '.github/workflows'.",
-    )
-
-    parser.add_argument(
-        "--reset",
-        action="store_true",
-        help="Reset template, remove all generated files",
-    )
-
-    parser.add_argument(
-        "-t",
-        "--project-type",
-        type=int,
-        choices=[t for t in range(len(PROJECT_TYPE))],
-        help=f"Type of project to create: {PROJECT_TYPE}",
-    )
-
-    def validate_project_name(name: str):
-        if not name.isidentifier():
-            raise ArgumentTypeError(f"Project name '{name}' must be a valid identifier")
-        return name
-
-    parser.add_argument(
-        "-n",
-        "--project-name",
-        type=validate_project_name,
-        help="Name of the project. Affects namespace and the include directory name.",
-    )
-
-    args = parser.parse_args()
-
-    if not (args.reset or args.remove_templates) and (
-        args.project_type is None or args.project_name is None
-    ):
-        parser.error(
-            "--project-type and --project-name are required unless "
-            "--remove-templates or --reset is specified"
-        )
-
+    print(get_linenos(".templates/common/.clangd", "---"))
+    exit(1)
+    args = parse_arguments()
     main(args)
